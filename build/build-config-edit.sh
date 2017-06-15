e6af3f8cd33f844ee8c2d8c491d1d4d33530994b
David Obando <daobando@microsoft.com>
2017-06-14 17:38:50 -0700

    OneBuild - one build to release them all (#7)

--- build/build-config-edit.sh
+++ build/build-config-edit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 bindir=$1
-unamestr=`uname`
+
+if [[ -z "${bindir}" ]]; then
+    bindir="$PWD/bin"
+fi
 
 sourcedir="./src/config-edit"
 if [[ ! -d "$sourcedir" ]]; then
@@ -8,22 +11,26 @@ if [[ ! -d "$sourcedir" ]]; then
 	exit -1
 fi
 
-if [[ "$unamestr" == "Linux" ]]; then
-    go_os="linux"
-elif [[ "$unamestr" == "Darwin" ]]; then
-    go_os="darwin"
-elif [[ "$unamestr" == "MSYS_NT-6.3" ]]; then
-    go_os="windows"
-    exe_extension=".exe"
-fi
+export CGO_ENABLED=0
+export GOARCH=amd64
+export GOPATH=$PWD
+echo "Go path = $GOPATH"
+for go_os in "linux" "windows" "darwin"
+do
+    if [[ "$go_os" == "windows" ]]; then
+        exe_extension=".exe"
+    else
+        exe_extension=""
+    fi
+    outputFile="${bindir}/config-edit-${go_os}${exe_extension}"
+    echo "Building ${outputFile}..."
+    export GOOS=$go_os
+    go build -o $outputFile $sourcedir
+    buildExitCode=$?
 
-outputFile="${bindir}/config-edit${exe_extension}"
-echo "Building ${outputFile}..."
-CGO_ENABLED=0 GOOS=$go_os go build -o $outputFile $sourcedir
-buildExitCode=$?
-
-if [[ $buildExitCode == 0 ]]; then
-    echo "Built ${outputFile} successfully"
-else
-    exit $buildExitCode
-fi
+    if [[ $buildExitCode == 0 ]]; then
+        echo "Built ${outputFile} successfully"
+    else
+        exit $buildExitCode
+    fi
+done
--- build/build-cred-helper.sh
+++ build/build-cred-helper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 bindir=$1
-tags=$2
-unamestr=`uname`
+
+if [[ -z "${bindir}" ]]; then
+    bindir="$PWD/bin"
+fi
 
 sourcedir="./src/docker-credential-acr"
 if [[ ! -d "$sourcedir" ]]; then
@@ -9,36 +11,33 @@ if [[ ! -d "$sourcedir" ]]; then
 	exit -1
 fi
 
-if [[ "$unamestr" == "Linux" ]]; then
-    osname="linux"
-    go_os="linux"
-elif [[ "$unamestr" == "Darwin" ]]; then
-    osname="osx"
-    go_os="darwin"
-elif [[ "$unamestr" == "MSYS_NT-6.3" ]]; then
-    osname="windows"
-    go_os="windows"
-    exe_extension=".exe"
-else
-    echo "OS $unamestr is not yet supported"
-    exit -1
+if [[ ! -z "${2}" ]]; then
+	buildtags="--tags '${2}'"
 fi
 
-if [[ ! -z "${tags}" ]]; then
-	buildtags="--tags '${tags}'"
-fi
-
-if [[ ! -z "$3" ]]; then
+if [[ ! -z "${3}" ]]; then
     outputSuffix="-${3}"
 fi
 
-outputFile="${bindir}/docker-credential-acr-${osname}${outputSuffix}${exe_extension}"
-echo "Building ${outputFile} ${buildtags}..."
-CGO_ENABLED=0 GOOS=$go_os go build -o $outputFile ${buildtags} $sourcedir
-buildExitCode=$?
+export CGO_ENABLED=0
+export GOARCH=amd64
+export GOPATH=$PWD
+for go_os in "linux" "windows" "darwin"
+do
+    if [[ "$go_os" == "windows" ]]; then
+        exe_extension=".exe"
+    else
+        exe_extension=""
+    fi
+    outputFile="${bindir}/docker-credential-acr-${go_os}${outputSuffix}${exe_extension}"
+    echo "Building ${outputFile} ${buildtags}..."
+    export GOOS=$go_os
+    go build -o $outputFile ${buildtags} $sourcedir
+    buildExitCode=$?
 
-if [[ $buildExitCode == 0 ]]; then
-    echo "Built ${outputFile} successfully"
-else
-    exit $buildExitCode
-fi
+    if [[ $buildExitCode == 0 ]]; then
+        echo "Built ${outputFile} successfully"
+    else
+        exit $buildExitCode
+    fi
+done
