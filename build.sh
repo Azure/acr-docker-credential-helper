cb33db75c537fbd252edbd1f58bce44a69e1bc7b
mickkael <19755421+mickkael@users.noreply.github.com>
2020-10-20 18:51:02 +0800

    commit from @odidev

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 buildImageName="acr-cred-helper-build-img"
 buildContainerName="acr-cred-helper-build"
-buildGoArch="amd64"
+if [ "$(uname -m)" == "aarch64" ]
+then
+   buildGoArch="arm64"
+else
+   buildGoArch="amd64"
+fi
 
 if [[ "$(docker images -q ${buildImageName} 2> /dev/null)" == "" ]]; then
     docker rmi -f ${buildImageName}
@@ -11,7 +16,7 @@ set -e
 ./build/build-clean.sh bin artifacts
 
 docker build -t ${buildImageName} .
-docker run --name ${buildContainerName} ${buildImageName} -e GOARCH=$buildGoArch
+docker run --name ${buildContainerName} -e GOARCH=$buildGoArch ${buildImageName}
 docker cp ${buildContainerName}:/build-root/artifacts artifacts
 
 docker rm ${buildContainerName}
